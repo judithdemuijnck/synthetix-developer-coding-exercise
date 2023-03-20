177f1c86ec8bc90ff73d6c9fbba624d6436d25c7
judithdemuijnck <61882827+judithdemuijnck@users.noreply.github.com>
2023-03-20 19:02:06 +0000

    create useEffect to verify session/token

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
 import './App.css';
+import useToken from './utils/useToken.js';
+import configureAxios from './utils/configAxios';
+import { useEffect, useState } from 'react';
+
 
 function App() {
+  const { token, initializeSession, verifySession, deleteSession } = useToken()
+  const axios = configureAxios(token)
+
+  useEffect(() => {
+    const getSession = async () => {
+      if (token) {
+        await verifySession()
+      } else {
+        await initializeSession()
+      }
+    }
+    getSession()
+  }, [])
+
+
+
   return (
     <div className="App">
 
@@ -9,3 +29,25 @@ function App() {
 }
 
 export default App;
+
+
+
+
+// make api call & check if anything else is needed
+// create search bar
+// display articles based on search terms
+// link to articles
+
+// components:
+// search bar
+// article
+
+//issues api
+// what channel to use
+// do i need a userid?
+
+
+
+// ERROR HANDLING AXIOS
+
+
